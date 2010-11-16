04ed74d6f9ec378b34af4462156bc0906551e708
sophiezjs@gmail.com <sophiezjs@gmail.com@4ac66662-7355-e678-26cc-cdeeae36cb60>
2010-11-16 22:58:53 +0000

    original version

---
+++ trunk/bib/auto/ref.el
@@ -0,0 +1,9 @@
+(TeX-add-style-hook "ref"
+ (lambda ()
+    (LaTeX-add-bibitems
+     "Magnusson:2002:simics-orig-paper"
+     "SPEC:2006"
+     "CACTI"
+     "NPB"
+     "Sun:2009:MRAM-L2-CMP")))
+
